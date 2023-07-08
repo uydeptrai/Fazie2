e4e7545a92026155f407eb92a7dba1b0d4da0404
uy <uy.hoang2000214@vnuk.edu.vn>
2023-07-09 05:35:22 +0700

    hihi

---
+++ js/main.js
@@ -0,0 +1,122 @@
+(function ($) {
+    "use strict";
+    
+    // Initiate the wowjs animation library
+    new WOW().init();
+    
+    // Initiate menu
+    $('#header').after('<div class="mobile-menu d-xl-none">');
+    $('.top-menu').clone().appendTo('.mobile-menu');
+    $('.mobile-menu-btn').click(function () {
+        $('.mobile-menu').stop().slideToggle();
+    });
+    
+    // Back to top button
+    $(window).scroll(function () {
+        if ($(this).scrollTop() > 100) {
+            $('.back-to-top').fadeIn('slow');
+        } else {
+            $('.back-to-top').fadeOut('slow');
+        }
+    });
+    $('.back-to-top').click(function () {
+        $('html, body').animate({scrollTop: 0}, 1500, 'easeInOutExpo');
+        return false;
+    });
+    
+    // Date and time picker
+    $('#date-1, #date-2, #date-3, #date-4').datetimepicker({
+        format: 'L'
+    });
+    $('#time-1, #time-2').datetimepicker({
+        format: 'LT'
+    });
+    
+    //Portfolio modal slider
+    $('.port-slider').delay(10000);
+    $('.port-slider').slick({
+        autoplay: true,
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        arrows: false,
+        fade: true,
+        asNavFor: '.port-slider-nav'
+    });
+    $('.port-slider-nav').slick({
+        autoplay: true,
+        slidesToShow: 5,
+        slidesToScroll: 1,
+        asNavFor: '.port-slider',
+        arrows: false,
+        dots: false,
+        centerMode: true,
+        focusOnSelect: true
+    });
+    
+    $('#popover-content-download').hide();
+    $("[data-toggle=popover]").each(function (e) {
+        $(this).popover({
+            html: true,
+            content: function () {
+                var id = $(this).attr('id')
+                return $('#popover-content-' + id).html();
+            }
+        });
+
+    });
+})(jQuery);
+let slideIndex = 1;
+showSlides(slideIndex);
+
+// Next/previous controls
+function plusSlides(n) {
+  showSlides(slideIndex += n);
+}
+
+// Thumbnail image controls
+function currentSlide(n) {
+  showSlides(slideIndex = n);
+}
+
+function showSlides(n) {
+  let i;
+  let slides = document.getElementsByClassName("mySlides");
+  let dots = document.getElementsByClassName("demo");
+  let captionText = document.getElementById("caption");
+  if (n > slides.length) {slideIndex = 1}
+  if (n < 1) {slideIndex = slides.length}
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";
+  }
+  for (i = 0; i < dots.length; i++) {
+    dots[i].className = dots[i].className.replace(" active", "");
+  }
+  slides[slideIndex-1].style.display = "block";
+  dots[slideIndex-1].className += " active";
+  captionText.innerHTML = dots[slideIndex-1].alt;
+}
+// Get the modal
+var modal = document.getElementById("myModal");
+
+// Get the button that opens the modal
+var btn = document.getElementById("myBtn");
+
+// Get the <span> element that closes the modal
+var span = document.getElementsByClassName("close")[0];
+
+// When the user clicks on the button, open the modal
+btn.onclick = function() {
+  modal.style.display = "block";
+}
+
+// When the user clicks on <span> (x), close the modal
+span.onclick = function() {
+  modal.style.display = "none";
+}
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function(event) {
+  if (event.target == modal) {
+    modal.style.display = "none";
+  }
+}
